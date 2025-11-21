e8e20bc69de92598c0cae4697c122efb37ed4429
chungchamchi <dinhchungkhtnk29@gmail.com>
2025-11-21 18:29:56 +0700

    chore: change mess title

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -9,10 +9,15 @@ type Props = {
 
 const Page = async ({ params }: Props) => {
   const { id } = await params;
-
+  const name = decodeURIComponent(id) || "My Friend";
   return (
     <>
-      <MetaTags />
+      <MetaTags
+        title={`
+        A Special Invitation for ${name}
+        `}
+        description={`Dear ${name}, we would be so happy if you could join us on our special day. Open this to see a little piece of our happiness!`}
+      />
       <Invitation name={decodeURIComponent(id) || "My Friend"}></Invitation>
     </>
   );
